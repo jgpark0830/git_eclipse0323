385a56b789781dc2a401f3d677108093054cd415
jgpark0830 <jgpark0830@gmail.com>
2020-03-23 16:42:26 +0900

    hotfix branch에서 저장점을 3개 더 가지기 위한 source추가

---
+++ dev_book/src/hotfix/HotFixTest3.java
@@ -0,0 +1,11 @@
+package hotfix;
+
+public class HotFixTest3 {
+
+	public static void main(String[] args) {
+	//hotfix branch에서 저장점을 3개 더 가지기 위한 source추가
+		System.out.println("hotfix branch에서 3번째로 추가한 소스임.");
+
+	}
+
+}
