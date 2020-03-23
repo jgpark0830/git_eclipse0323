8a6f74515985bd656dde384bbc04d4603238d2ac
jgpark0830 <jgpark0830@gmail.com>
2020-03-23 15:56:52 +0900

    hotfix branch에서 2번째로 추가한 소스임.

---
+++ dev_book/src/hotfix/HotFixTest2.java
@@ -0,0 +1,11 @@
+package hotfix;
+
+public class HotFixTest2 {
+
+	public static void main(String[] args) {
+	//hotfix branch에서 저장점을 한개 더 가지기 위한 source추가
+		System.out.println("hotfix branch에서 2번째로 추가한 소스임.");
+
+	}
+
+}
