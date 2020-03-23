c6cf3543735c08dc70336ae39b3a590069fd5bcc
jgpark0830 <jgpark0830@gmail.com>
2020-03-23 15:43:52 +0900

    hotfix branch로 파일 추가함 3시 43분

---
+++ dev_book/src/hotfix/HotFixTest.java
@@ -0,0 +1,11 @@
+package hotfix;
+
+public class HotFixTest {
+
+	public static void main(String[] args) {
+	//project명 -> 오른쪽 -> Remote -> Switch to -> New Branch -> hotfix
+		System.out.println("hotfix branch에서 추가한 소스임.");
+
+	}
+
+}
