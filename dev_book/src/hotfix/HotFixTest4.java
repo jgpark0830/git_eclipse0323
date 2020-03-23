7fc86f2a3d87b3b38a4f9f3f9c01a7a37ae70bed
jgpark0830 <jgpark0830@gmail.com>
2020-03-23 16:57:59 +0900

    hotfix branch에서 저장점을 4개 더 가지기 위한 source추가

---
+++ dev_book/src/hotfix/HotFixTest4.java
@@ -0,0 +1,11 @@
+package hotfix;
+
+public class HotFixTest4 {
+
+	public static void main(String[] args) {
+	//hotfix branch에서 저장점을 4개 더 가지기 위한 source추가
+		System.out.println("hotfix branch에서 4번째로 추가한 소스임.");
+
+	}
+
+}
